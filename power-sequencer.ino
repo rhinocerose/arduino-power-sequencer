ee1ed03e39b7ec0442906e65230b1814e840a34b
rhinocerose <ashar.k.latif@gmail.com>
2020-10-03 17:38:13 -0400

    quick commit

--- power-sequencer.ino
+++ power-sequencer.ino
@@ -1,8 +1,16 @@
 #define TEST_MODE
+#define __NANO__
+//#define __UNO__
 
-#define SPEAKER_RELAY   3
-#define MIXER_RELAY     4
-#define SYNTH_RELAY     5
+#if defined(__NANO__)
+    #define SPEAKER_RELAY   3
+    #define MIXER_RELAY     4
+    #define SYNTH_RELAY     5
+#elif defined(__UNO__)
+    #define SPEAKER_RELAY   3
+    #define MIXER_RELAY     4
+    #define SYNTH_RELAY     5
+#endif
 
 #define RELAY_OFF       0
 #define RELAY_ON        1
@@ -11,20 +19,13 @@ void setup() {
     digitalWrite(SPEAKER_RELAY, RELAY_OFF);
     digitalWrite(MIXER_RELAY, RELAY_OFF);
     digitalWrite(SYNTH_RELAY, RELAY_OFF);
-    #ifdef TEST_MODE
+    #if defined(TEST_MODE)
         pinMode(LED_BUILTIN, OUTPUT);
     #endif
 
     pinMode(SPEAKER_RELAY, OUTPUT);
     pinMode(MIXER_RELAY, OUTPUT);
     pinMode(SYNTH_RELAY, OUTPUT);
-}
-
-void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
 }
 
 void turn_on_speakers(void) {
@@ -49,10 +50,22 @@ void turn_off_mixer(void) {
 }
 
 void turn_on_synths(void) {
-    if (digitalRead(MIXER_RELAY) == HIGH) {
+    if ((digitalRead(MIXER_RELAY) == HIGH) || (digitalRead(SPEAKER_RELAY) == HIGH)) {
         turn_off_mixer();
         delay(500);
         digitalWrite(SYNTH_RELAY, RELAY_ON);
         turn_on_speakers();
     }
+}
+
+void turn_off_synths(void) {
+    turn_off_mixer();
+    digitalWrite(SYNTH_RELAY, RELAY_OFF);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
 }
